2c24344ed73214d55b151abdfc6aac1c5351cd4e
KevinPham <kevinpham15@gmail.com>
2017-11-01 16:58:41 -0700

    completed

---
+++ Deck_of_cards.playground/Contents.swift
@@ -0,0 +1,89 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+struct Card {
+    let Color: String
+    let Roll: Int
+
+}
+
+
+
+
+class Deck {
+    var cards = [Card]()
+    init(){
+        for _ in 0...9 {
+            self.cards.append(Card(Color: "Blue", Roll: Int(arc4random_uniform(2)+1)))
+            self.cards.append(Card(Color: "Red", Roll: Int(arc4random_uniform(2)+3)))
+            self.cards.append(Card(Color: "Green", Roll: Int(arc4random_uniform(2)+5)))
+        }
+    }
+
+func deal() -> Card? {
+    if self.cards.count > 0 {
+        return self.cards.remove(at: 0)
+    }
+    else {
+        return nil
+    }
+}
+
+func isEmpty() -> Bool {
+    if self.cards.count > 0{
+        return false
+    }
+    else{
+        return true
+    }
+}
+
+func shuffle(){
+    for i in stride(from: self.cards.count-1, to: 0, by: -1){
+        //counting from 30 cards to 0, this method will include 0
+        let swapIndex = Int(arc4random_uniform(UInt32(i)))
+        let temp = self.cards[i]
+        self.cards[i] = self.cards[swapIndex]
+        self.cards[swapIndex] = temp
+    }
+}
+
+
+}
+
+class Player {
+    var name: String
+    var hand = [Card]()
+    
+    init(name: String){
+        self.name = name
+    }
+    
+    func draw(deck:Deck) -> Card? {
+        if let drawnCard = deck.deal(){
+            self.hand.append(drawnCard)
+            return drawnCard
+        } else {
+            return nil
+        }
+    }
+    
+    func rollDice() -> Int {
+        let random = arc4random_uniform(6)+1
+        return Int(random)
+    }
+    
+    func matchingCards(Roll: Int, Color: String) -> Int {
+        var count: Int = 0
+        for x in hand {
+            if (Roll == x.Roll && Color == x.Color){
+                count += 1
+            }
+        }
+        print(count)
+        return count
+    }
+}
